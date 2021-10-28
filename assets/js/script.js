54af519b2a2b16f70314f9b3095b29b6b657f996
Devin Winando <devin.winando13@gmail.com>
2021-10-28 21:36:16 +0700

    add section 2 (features)

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 6,
+  grabCursor: true,
+  speed: 800,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
