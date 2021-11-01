ecf97c07bdece26669ab22b389cffc19ef45b8e6
Devin Winando <devin.winando13@gmail.com>
2021-11-01 21:32:55 +0700

    responsive mobile #1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 6,
   grabCursor: true,
   speed: 800,
   autoplay: {
     delay: 1000,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+  },
 });
+
+function init() {
+  const nama = "Devin";
+
+  function tampil() {
+    console.log(nama);
+  }
+
+  return tampil;
+}
+
+init();
